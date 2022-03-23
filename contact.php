d3e0ffe93effa81ddc69af4be84219c6a7e772d6
Pritam bag <51673156+developerpritam@users.noreply.github.com>
2022-03-23 17:29:56 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,31 +1,14 @@
 <?php
-//get data from form  
-if (isset($_POST["submit"])){
-    $name = $_POST['name'];
-    $email= $_POST['email'];
-    $subject= $_POST['subject'];
-    $message= $_POST['message'];
-    echo $name;
-}
+$name = $_REQUEST['name'];
+$email = $_REQUEST['email'];
+$subject = $_REQUEST['subject'];
+$message = $_REQUEST['message'];
 
-
-$to = "webdevpritam@gmail.com";
-$subject = $message;
-
-$message = "email: {$email}" $message;
-
-// Always set content-type when sending HTML email
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-
-// More headers
-$headers .= 'From: webdevpritam@gmail.com';
-
-$mail = mail($to,$subject,$message,$headers);
-
-if($mail){
-    echo "<script>alert('Mail Send.');</script>";
+if(empty($name) || empty($email) || empty($message)){
+    echo "<script>alert('Please fil')</script>";
 }else{
-    echo "<script>alert('Mail Not Send.');</script>";
+mail("pritambag426@gmail.com", "webtech message", "$message");
+echo "<script>alert('Message send successfully')</script>";
+window.history.log(-1);
 }
 ?>
