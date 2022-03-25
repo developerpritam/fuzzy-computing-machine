6e05f2450f5621224e0aff7022c5c6fceb5782d6
Pritam bag <51673156+developerpritam@users.noreply.github.com>
2022-03-25 10:14:23 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -7,8 +7,9 @@ $message = $_REQUEST['message'];
 if(empty($name) || empty($email) || empty($message)){
     echo "<script>alert('Please fil')</script>";
 }else{
-mail("pritambag426@gmail.com", "webtech message", "$message");
-echo "<script>alert('Message send successfully')</script>";
+mail("pritambag426@gmail.com", $subject, $message, "From: $name <$email>");
+echo "<script>alert('Message send successfully')
 window.history.log(-1);
+</script>";
 }
 ?>
